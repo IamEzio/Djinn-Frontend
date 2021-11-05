5cff034a85056dfb0f18944770af93a6439579a3
Anshuman <anshumanmaurya111@gmail.com>
2021-11-05 19:04:01 +0530

    added checkout Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 import Home from "../views/Home.vue";
 import Product from "../views/Product.vue";
 import Category from "../views/Category.vue";
@@ -6,6 +7,8 @@ import Search from "../views/Search.vue";
 import Cart from "../views/Cart.vue";
 import SignUp from "../views/SignUp.vue";
 import Login from "../views/Login.vue";
+import MyAccount from "../views/MyAccount.vue";
+import Checkout from "../views/Checkout.vue";
 const routes = [
   {
     path: "/",
@@ -42,6 +45,22 @@ const routes = [
     component: Login,
   },
   {
+    path: "/my-account",
+    name: "MyAccount",
+    component: MyAccount,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/cart/checkout",
+    name: "Checkout",
+    component: Checkout,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
     path: "/:category_slug/:product_slug",
     name: "Product",
     component: Product,
@@ -56,6 +75,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (
+    to.matched.some((record) => record.meta.requireLogin) &&
+    !store.state.isAuthenticated
+  ) {
+    next({ name: "LogIn", query: { to: to.path } });
+  } else {
+    next();
+  }
 });
 
 export default router;
